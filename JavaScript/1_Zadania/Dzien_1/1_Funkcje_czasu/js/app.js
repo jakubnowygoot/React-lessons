d607933753dd2dc7df73c3225187ebdef8d87ec1
jakubnowygoot <jakubsiepiela@gmail.com>
2022-01-30 12:36:44 +0100

    zadanka

--- JavaScript/1_Zadania/Dzien_1/1_Funkcje_czasu/js/app.js
+++ JavaScript/1_Zadania/Dzien_1/1_Funkcje_czasu/js/app.js
@@ -1,1 +1,28 @@
+let timeout = setTimeout(function () {
+    console.log('Zadziałam za 10 minut!');
+}, 600000);  //funkcja zadziala za 10 minut
+
+let interval = setInterval(function () {
+    console.log('Działam co 13 minut!');
+}, 780000);  //funkcja dziala co 13 minut
+
+
+
+
+
+function countHello(n) {
+
+    if (n=1, n>=10, n++) {
+
+        let interval = setInterval(function () {
+            console.log('Działam co sekundę!');
+        }, 1000);
+
+
+    } else {
+        console.log("podaj liczbe miedzy 1-10")
+    }
+
+}
+
 
