d420e5e64be2be0e93a29e31292d9a181d59100a
jakubnowygoot <jakubsiepiela@gmail.com>
2023-01-22 14:17:17 +0100

    expanseApp

--- lesson_01/src/App.js
+++ lesson_01/src/App.js
@@ -1,12 +1,39 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expanses from "./components/Expanses/Expanse";
+import NewExpanses from "./components/NewExpanses/NewExpanses";
 
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expanses = [
+        {
+            id: "e1",
+            title: "Toilet Paper",
+            amount: 245,
+            date: new Date(2020, 5, 2),
+        },
+        {
+            id: "e2",
+            title: "New TV",
+            amount: 144.23,
+            date: new Date(2021, 4, 12),
+        },
+        {
+            id: "e3",
+            title: "New Car",
+            amount: 2000,
+            date: new Date(2022, 12, 29),
+        },
+        {
+            id: "e4",
+            title: "Car Insurance",
+            amount: 120.23,
+            date: new Date(2021, 2, 14),
+        }
+    ];
+    return (
+        <div>
+            <NewExpanses/>
+            <Expanses items={expanses}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ lesson_01/src/components/NewExpanses/NewExpanses.js
@@ -0,0 +1,10 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpanses.css";
+
+const NewExpaneses = () => {
+    return <div className="new-expense">
+        <ExpenseForm/>
+    </div>
+}
+
+export default NewExpaneses;
