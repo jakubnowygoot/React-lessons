fa7ccd3df6ae0a150842918d25d647b5b74b3ab5
jakubnowygoot <jakubsiepiela@gmail.com>
2023-01-25 16:15:52 +0100

    ExpensesApp

--- lesson_01/src/App.js
+++ lesson_01/src/App.js
@@ -1,44 +1,47 @@
-import Expanses from "./components/Expanses/Expanse";
-import NewExpanses from "./components/NewExpanses/NewExpanses";
 
-function App() {
-    const expanses = [
-        {
-            id: "e1",
-            title: "Toilet Paper",
-            amount: 245,
-            date: new Date(2020, 5, 2),
-        },
-        {
-            id: "e2",
-            title: "New TV",
-            amount: 144.23,
-            date: new Date(2021, 4, 12),
-        },
-        {
-            id: "e3",
-            title: "New Car",
-            amount: 2000,
-            date: new Date(2022, 12, 29),
-        },
-        {
-            id: "e4",
-            title: "Car Insurance",
-            amount: 120.23,
-            date: new Date(2021, 2, 14),
-        }
-    ];
 
-    const addExpanseHandler = (expanse) => {
-        console.log("Im in App");
-        console.log(expanse);
-    }
+import React, { useState } from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
+
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+    const addExpenseHandler = (expense) => {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    };
+
     return (
         <div>
-            <NewExpanses onAddExpanse={addExpanseHandler}/>
-            <Expanses items={expanses}/>
+            <NewExpense onAddExpense={addExpenseHandler} />
+            <Expenses items={expenses} />
         </div>
     );
-}
+};
 
 export default App;
