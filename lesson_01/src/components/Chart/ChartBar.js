47bcf4ab35ec048626dbcc5fded5413dff15835e
jakubnowygoot <jakubsiepiela@gmail.com>
2023-01-29 16:49:20 +0100

    App_3

--- lesson_01/src/components/Chart/ChartBar.js
+++ lesson_01/src/components/Chart/ChartBar.js
@@ -15,7 +15,7 @@ const ChartBar = (props) => {
                 <div
                     className='chart-bar__fill'
                     style={{ height: barFillHeight }}
-                ></div>
+                />
             </div>
             <div className='chart-bar__label'>{props.label}</div>
         </div>
